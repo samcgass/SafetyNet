15054b79a84cae1ba30f27ede9fe134dcc63349e
Sam Gass <samcgass@gmail.com>
2020-09-03 21:02:26 -0500

    Added navigation

--- SafetyNet/Check In/MayICallView.swift
+++ SafetyNet/Check In/MayICallView.swift
@@ -11,10 +11,16 @@ import SwiftUI
 struct MayICallView: View {
     
     @Binding var tab: Int
+    @State private var emergency: Bool = false
     
     var body: some View {
         
         VStack {
+            
+            NavigationLink(destination: Emergency(),
+                           isActive: $emergency) {
+                            EmptyView()
+            }
             
             Text("May I call someone to help?")
                 .font(.title)
@@ -31,12 +37,16 @@ struct MayICallView: View {
             
             ButtonView(buttonLabel: "Yes, call them please.",
                        buttonColor: Color(red: 100/255, green: 200/255, blue: 20/255),
-                       buttonAction: {})
+                       buttonAction: {
+                        self.emergency = true
+            })
             
             
             ButtonView(buttonLabel: "No, I will seek help",
                        buttonColor: Color(red: 210/255, green: 34/255, blue: 45/255),
-                       buttonAction: {})
+                       buttonAction: {
+                        self.tab = 2
+            })
             
         }
         
