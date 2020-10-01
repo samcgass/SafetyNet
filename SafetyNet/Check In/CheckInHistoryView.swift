a15c6792fc9b619b0589b240dae9824efdb13f3a
Sam Gass <samcgass@gmail.com>
2020-09-30 22:30:24 -0500

    added checkin history file

---
+++ SafetyNet/Check In/CheckInHistoryView.swift
@@ -0,0 +1,21 @@
+//
+//  CheckInHistoryView.swift
+//  SafetyNet
+//
+//  Created by Sam Gass on 9/29/20.
+//  Copyright © 2020 SafetyNet. All rights reserved.
+//
+
+import SwiftUI
+
+struct CheckInHistoryView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CheckInHistoryView_Previews: PreviewProvider {
+    static var previews: some View {
+        CheckInHistoryView()
+    }
+}
