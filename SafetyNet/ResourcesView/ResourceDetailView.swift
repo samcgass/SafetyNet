56f7d5745a7256d501b1cb46bd2ac06be4c002aa
Sam Gass <samcgass@gmail.com>
2020-10-17 22:37:52 -0500

    added resource detail view

---
+++ SafetyNet/ResourcesView/ResourceDetailView.swift
@@ -0,0 +1,26 @@
+//
+//  ResourceDetailView.swift
+//  SafetyNet
+//
+//  Created by Sam Gass on 10/17/20.
+//  Copyright © 2020 SafetyNet. All rights reserved.
+//
+
+import SwiftUI
+
+struct ResourceDetailView: View {
+    var id: Int32
+    
+    var body: some View {
+//        let db = openDatabase()
+//        let resource = getResource(db: db, id: id)
+//        EmptyView()
+        Text("hello")
+    }
+}
+
+struct ResourceDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        ResourceDetailView(id: 1)
+    }
+}
